d4b2f4ff0641668a1087618720bc92d0f17863f6
path-finder2002 <asohp1hehejj@gmail.com>
2025-09-23 12:56:27 +0900

    MVPスライドコンポーネントのデータセットファクトリを削除

--- mvp/components/closing_mvp.js
+++
@@ -1,9 +0,0 @@
-/**
- * MVP dataset factory for closing slide component.
- */
-function createClosingSlideData(overrides = {}) {
-  return Object.assign({
-    type: 'closing',
-    notes: 'MVPのご確認ありがとうございました。フィードバックをお待ちしています。'
-  }, overrides || {});
-}
